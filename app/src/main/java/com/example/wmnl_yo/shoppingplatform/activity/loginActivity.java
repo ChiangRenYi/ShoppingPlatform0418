95044d9a622dedbeb9f84450b5a660530e58c496
WMNL-Jimmy <skybabysuki@hotmail.com>
2018-04-18 15:31:52 +0800

    test

--- app/src/main/java/com/example/wmnl_yo/shoppingplatform/activity/loginActivity.java
+++ app/src/main/java/com/example/wmnl_yo/shoppingplatform/activity/loginActivity.java
@@ -171,6 +171,6 @@ public class loginActivity extends Activity {
     }
 
     void testOriginalUserPush() {
-        Log.d("test","測試用原本的使用者上傳程式");
+        Log.d("test","測試用原本的使用者上傳程式~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 }
